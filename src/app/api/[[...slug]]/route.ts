70a5c5b60a0b1613242ad59e0df3ea8145c9b48f
Muhammadali <boburjonr934@gmail.com>
2025-12-12 09:52:12 +0500

    connectiong to database

--- src/app/api/[[...slug]]/route.ts
+++ src/app/api/[[...slug]]/route.ts
@@ -1,7 +1,19 @@
+import { redis } from '@/lib/redis';
 import { Elysia, t } from 'elysia'
+import { nanoid } from 'nanoid';
+const ROOM_SECONDS = 60 * 10;
 
 const room = new Elysia({ prefix: '/room' })
-    .post("/create", () => { console.log("Hellooo room") })
+    .post("/create", async () => {
+        const roomId = nanoid();
+        await redis.hset(`meta:${roomId}`, {
+            connected: [],
+            createdAt: Date.now(),
+        });
+
+        await redis.expire(`meta:${roomId}`, ROOM_SECONDS);
+        return { roomId };
+    })
 
 const app = new Elysia({ prefix: '/api' }).use(room);
 
